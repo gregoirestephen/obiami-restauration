cab2aabbaa4b4ed15fd41955ae5bb2e1831be7c1
gregoire stephen <gregoirerichard91@gmail.com>
2021-03-02 11:19:23 +0000

    image code change

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -28,7 +28,7 @@ class ArticleController extends Controller
     public function image_upload(){
         $temp=request()->file('image')->store('uploads','public');
         // $image = Image::make(public_path("storage/" . $temp))->fit(600,600);
-        $image= Image::make("/public/storage/" . $temp);
+        $image= Image::make(base_path("/public/storage/") . $temp);
         $image->resize(600,600);
         $image->save();
         return $temp;
